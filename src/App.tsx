db58d55b50b5b3c3690244a3454fbdc8d9ea3956
renbran <renbranmadelo@gmail.com>
2025-11-22 20:47:17 +0400

    fix: Update routing to show new hero section on root domain - Move ComingSoon to /coming-soon path

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ComingSoon />} />
-          <Route path="/home" element={<Index />} />
+          <Route path="/" element={<Index />} />
+          <Route path="/coming-soon" element={<ComingSoon />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
